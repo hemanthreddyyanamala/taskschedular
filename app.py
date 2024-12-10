e49d9c63c344744de8fd5d940f4a62bf93877b8d
hemanthreddyyanamala <149450489+hemanthreddyyanamala@users.noreply.github.com>
2024-12-10 19:03:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ clf = LogisticRegression(random_state=0, max_iter=10000)
 # Preprocess the data
 tags = []
 patterns = []
-for intent in intents['intents']:
+for intent in intents:
     for pattern in intent['patterns']:
         tags.append(intent['tag'])
         patterns.append(pattern)
